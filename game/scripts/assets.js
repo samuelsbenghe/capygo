90483c11fb3c2f07bf140b03f9d46fecdc536eb0
Edith-Samuel Sbenghe <samuel.sbenghe@gmail.com>
2024-12-31 18:58:27 +0000

    moved assets

--- game/scripts/assets.js
+++ game/scripts/assets.js
@@ -1,1 +1,27 @@
 let fontAtma, imgLogo;
+
+function AssetPreload() {
+  // Load Assets
+  fontAtma = loadFont("assets/Atma-Regular.ttf");
+  imgLogo = loadImage("assets/logo.png");
+
+  // Load Obstacles
+  obstacle1 = loadImage("assets/cactus1.png");
+  obstacle2 = loadImage("assets/cactus1.png");
+
+  // Load Sprites
+  walk1 = loadImage("assets/walk1.png");
+  walk2 = loadImage("assets/walk2.png");
+  walk3 = loadImage("assets/walk3.png");
+  walk4 = loadImage("assets/walk4.png");
+  walk5 = loadImage("assets/walk5.png");
+}
+
+function AssetSetup() {
+  // Setup Animations
+  animationWalk = [walk1, walk2, walk3, walk4, walk5];
+  currentFrame = walk1;
+
+  // Setup Obstacles
+  obstacleOptions = [obstacle1, obstacle2];
+}
--- game/scripts/sketch.js
+++ game/scripts/sketch.js
@@ -1,30 +1,13 @@
 function preload() {
-  // Load Assets
-  fontAtma = loadFont("assets/Atma-Regular.ttf");
-  imgLogo = loadImage("assets/logo.png");
-
-  // Load Obstacles
-  obstacle1 = loadImage("assets/cactus1.png");
-  obstacle2 = loadImage("assets/cactus1.png");
-
-  // Load Sprites
-  walk1 = loadImage("assets/walk1.png");
-  walk2 = loadImage("assets/walk2.png");
-  walk3 = loadImage("assets/walk3.png");
-  walk4 = loadImage("assets/walk4.png");
-  walk5 = loadImage("assets/walk5.png");
+  AssetPreload();
 }
 
 function setup() {
   createCanvas(CONFIG.VIEWPORT.width, CONFIG.VIEWPORT.height);
   frameRate(60);
 
-  // Setup Animations
-  animationWalk = [walk1, walk2, walk3, walk4, walk5];
-  currentFrame = walk1;
-
-  // Setup Obstacles
-  obstacleOptions = [obstacle1, obstacle2];
+  // Setup Assets
+  AssetSetup();
 
   // Setup Scenes
   sceneManager = new SceneManager();
